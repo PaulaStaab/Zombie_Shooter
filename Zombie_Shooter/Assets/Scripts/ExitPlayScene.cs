6f927720a4274f4fbfe045fe4ca16ba7d725edaa
BPKV444\Teilnehmer <paula.staab@web.de>
2026-02-04 10:34:10 +0100

    ADD: Tilemap ADD: Assets ADD: Power-Up UPD: PauseMenue, GameScene

---
+++ Zombie_Shooter/Assets/Scripts/ExitPlayScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ExitPlayScene : MonoBehaviour
+{
+    [Header("Buttons (Drag hier rein)")]
+    public Button LobbyButton;
+
+    void Start()
+    {
+        // Listener für die Buttons setzen
+        if (LobbyButton != null)
+            LobbyButton.onClick.AddListener(NewGame);
+    }
+
+    // "New Game" / Restart: Einfach die Game-Scene laden
+    public void NewGame()
+    {
+        SceneManager.LoadScene("StartScreen");  // Laden der Game-Szene
+    }
+}

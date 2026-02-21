304c041d13b1c91b14cef30bc4a97d7963573b35
BPKV444\Teilnehmer <paula.staab@web.de>
2026-02-21 13:10:16 +0100

    UPD: HealthBar Player und PowerUp Skript

--- Assets/Scripts/PowerUp.cs
+++ Assets/Scripts/PowerUp.cs
@@ -5,6 +5,7 @@ public class PowerUp : MonoBehaviour
 {
     public ParticleSystem collectEffect;  // Partikel reinziehen!
     public int healAmount = 100;
+    public Slider playerHealthSlider;  // Slider der HealthBar des Players hier zuweisen!
 
     void Update()
     {
@@ -22,8 +23,24 @@ public class PowerUp : MonoBehaviour
                 Debug.Log(healAmount);
             }
 
+            // UpdateHealthBar Funktion: Setzt Slider auf 100% (voll)
+            UpdateHealthBar();
+
             if (collectEffect != null) collectEffect.Play();
             Destroy(gameObject, 0.3f);
+        }
+    }
+
+    void UpdateHealthBar()
+    {
+        if (playerHealthSlider != null)
+        {
+            playerHealthSlider.value = playerHealthSlider.maxValue;  // Auf Maximum (100%)
+            Debug.Log("HealthBar auf 100% gesetzt!");
+        }
+        else
+        {
+            Debug.LogWarning("playerHealthSlider ist null! Im Inspector zuweisen.");
         }
     }
 }
